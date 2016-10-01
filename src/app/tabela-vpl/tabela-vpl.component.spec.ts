de660bf43d9535ebc2efd8773916429a3608b167
luishenriquepp <peppyno@gmail.com>
2016-09-30 22:45:35 -0300

    Criação de componente e de testes unitários para exibir o fluxo de caixa do financiamento

---
+++ src/app/tabela-vpl/tabela-vpl.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TabelaVplComponent } from './tabela-vpl.component';
+
+describe('Component: TabelaVpl', () => {
+  it('should create an instance', () => {
+    let component = new TabelaVplComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tabela-vpl/tabela-vpl.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Usuario } from '../models/usuario';
+import { Financiamento } from'../models/financiamento';
+
+@Component({
+  selector: 'app-tabela-vpl',
+  templateUrl: './tabela-vpl.component.html',
+  styleUrls: ['./tabela-vpl.component.css']
+})
+export class TabelaVplComponent implements OnInit { 
+
+  @Input() financiamento: Financiamento;
+
+  ngOnInit() {
+    this.financiamento = new Financiamento(new Usuario());
+  }
+}
