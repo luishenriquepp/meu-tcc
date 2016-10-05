1a81615243a6b42140a7321b11073800401ae5c8
luishenriquepp <peppyno@gmail.com>
2016-10-04 22:09:36 -0300

    criando um componente para armazenar o gráfico de evolução do patrimônio pelo tempo

---
+++ src/app/grafico-financiamento/grafico-financiamento.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GraficoFinanciamentoComponent } from './grafico-financiamento.component';
+
+describe('Component: GraficoFinanciamento', () => {
+  it('should create an instance', () => {
+    let component = new GraficoFinanciamentoComponent();
+    expect(component).toBeTruthy();
+  });
+});
