cf5e37468a5f7d458197a0311d52f82d82e3d695
luishenriquepp <peppyno@gmail.com>
2017-01-02 10:24:43 -0200

    Configuracao da propriedade step dos elementos de porcentagem

--- src/app/utils/financiamento-factory.spec.ts
+++ src/app/utils/financiamento-factory.spec.ts
@@ -1,10 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
-import { addProviders, async, inject } from '@angular/core/testing';
+import { async, inject } from '@angular/core/testing';
 import {FinanciamentoFactory} from './financiamento-factory';
+import {FinanciamentoFgtsConfig} from '../models/financiamento-fgts-config';
+import {FinanciamentoConfig} from '../models/financiamento-config';
+import {Usuario} from '../models/usuario';
 
 describe('FinanciamentoFactory', () => {
   it('should create an instance', () => {
-    expect(new FinanciamentoFactory()).toBeTruthy();
+    let fgtsConfig = new FinanciamentoFgtsConfig();
+    let financiamentoConfig = new FinanciamentoConfig(fgtsConfig);
+    let user = new Usuario();
+    expect(new FinanciamentoFactory(user, financiamentoConfig)).toBeTruthy();
   });
 });
