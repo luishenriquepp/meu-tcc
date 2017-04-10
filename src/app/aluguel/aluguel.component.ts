26e4a14a3e174f3cabed86364c5fdcd209d2767a
luishenriquepp <peppyno@gmail.com>
2017-04-09 22:02:56 -0300

    Printing an example on the screen

--- src/app/aluguel/aluguel.component.ts
+++ src/app/aluguel/aluguel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Comparador, Aluguel, Investimento, ExtratoAluguel } from '../models/aluguel/aluguel';
+import { FinanciamentoBuilder } from '../models/builders/financiamento-builder';
 
 @Component({
   selector: 'app-aluguel',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AluguelComponent implements OnInit {
 
+  extrato: Array<ExtratoAluguel>
+  
   constructor() { }
 
   ngOnInit() {
+    let comparador: Comparador;
+    let investimento = new Investimento(40000);
+    let aluguel = new Aluguel(1400);
+    let fgts = new Investimento(5000);
+
+    let builder = new FinanciamentoBuilder();
+    let finanSemFgts = builder.GetFinanciamento(48, 2000, 50);
+    
+    comparador = new Comparador(investimento, aluguel, finanSemFgts, fgts);
+    comparador.Processar();
+
+    this.extrato = comparador.Gerenciador.ExtratoAluguel;
   }
 
 }
