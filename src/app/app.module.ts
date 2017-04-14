e1c290ac43ea1fb2ac833472b4b65829bbf33903
luishenriquepp <peppyno@gmail.com>
2017-04-14 15:07:16 -0300

    Changing the locale to pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
@@ -44,7 +44,12 @@ import { FgtsComponent } from './fgts/fgts.component';
     HttpModule,
     RouterModule.forRoot(Routes)
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
