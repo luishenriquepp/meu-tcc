ad8aee7a518fd4df3617344e3acb46b8d67a6e34
luishenriquepp <peppyno@gmail.com>
2017-04-23 00:42:48 -0300

    Improving the html at global-configuration file

--- src/app/global-configuration/global-configuration.component.ts
+++ src/app/global-configuration/global-configuration.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {GlobalConfiguration} from '../models/global-configuration';
+import {ConfigurationService} from '../services/configuration-service';
 
 @Component({
   selector: 'app-global-configuration',
   templateUrl: './global-configuration.component.html',
-  styleUrls: ['./global-configuration.component.css']
+  styleUrls: ['./global-configuration.component.css'],
+  providers: [ConfigurationService]
 })
 export class GlobalConfigurationComponent implements OnInit {
 
-  constructor() { }
+  private configuration: GlobalConfiguration;
+  
+  constructor(private conService: ConfigurationService) { }
 
   ngOnInit() {
+    this.configuration = this.conService.Busca();
   }
-
 }
--- src/app/models/global-configuration.ts
+++ src/app/models/global-configuration.ts
@@ -1,6 +1,5 @@
 export class GlobalConfiguration {
-    
-    private _referencial : number;
+    private _referencial : number = 0.001708;
     public get Referencial() : number {
         return this._referencial;
     }
@@ -8,7 +7,7 @@ export class GlobalConfiguration {
         this._referencial = v;
     }
         
-    private _interna : number;
+    private _interna : number = 0.00848891;
     public get Interna() : number {
         return this._interna;
     }
@@ -16,7 +15,7 @@ export class GlobalConfiguration {
         this._interna = v;
     }
     
-    private _rentabilidade : number;
+    private _rentabilidade : number = 0.11;
     public get Rentabilidade() : number {
         return this._rentabilidade;
     }
@@ -24,7 +23,7 @@ export class GlobalConfiguration {
         this._rentabilidade = v;
     }
     
-    private _imovel : number;
+    private _imovel : number = 0.001;
     public get Imovel() : number {
         return this._imovel;
     }
@@ -32,11 +31,35 @@ export class GlobalConfiguration {
         this._imovel = v;
     }
     
-    private _fundo : number;
+    private _fundo : number = 0.003;
     public get Fundo() : number {
         return this._fundo;
     }
     public set Fundo(v : number) {
         this._fundo = v;
-    }        
+    }
+    
+    private _aluguel : number = 0.08;
+    public get Aluguel() : number {
+        return this._aluguel;
+    }
+    public set Aluguel(v : number) {
+        this._aluguel = v;
+    }
+    
+    private _impostoRenda : number  = 0.017;
+    public get ImpostoRenda() : number {
+        return this._impostoRenda;
+    }
+    public set ImpostoRenda(v : number) {
+        this._impostoRenda = v;
+    }
+
+    public RentabilidadeLiquida(): number {
+        return (this._rentabilidade*(1-this._impostoRenda));
+    }
+
+    public RentabilidadeLiquidaMensal(): number {
+        return Math.pow((1+this.Rentabilidade),1/12)-1
+    }      
 }
