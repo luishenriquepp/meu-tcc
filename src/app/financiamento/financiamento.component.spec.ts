f1932a6d2fa674cbadbfa366274ecc332ebc83f3
Luis Henrique Pacheco Paim <peppyno@gmail.com>
2017-05-22 12:39:49 -0300

    Solving unit tests that depends on others library

--- src/app/financiamento/financiamento.component.spec.ts
+++ src/app/financiamento/financiamento.component.spec.ts
@@ -3,21 +3,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { FinanciamentoComponent } from './financiamento.component';
 import {FinanciamentoProcessorService} from '../services/financiamento-processor-service';
-import {ConfigurationService} from '../services/configuration-service';
 
 describe('Component: Financiamento', () => {
   
   let processorService: FinanciamentoProcessorService;
-  let configurationService: ConfigurationService;
 
   beforeEach(() => {
 
-    configurationService = new ConfigurationService();
-    processorService = new FinanciamentoProcessorService(configurationService);
+    processorService = new FinanciamentoProcessorService();
     
     TestBed.configureTestingModule({
       providers: [
-        { provide: ConfigurationService, useClass: configurationService }]
+        { provide: processorService, useClass: processorService }]
     })
     TestBed.compileComponents();
   })
