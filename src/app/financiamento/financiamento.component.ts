20578455a0146832dc66d91998129f6b00cb6d1a
Luis Henrique Pacheco Paim <peppyno@gmail.com>
2017-06-06 18:53:51 -0300

    A better submenu for financiamento after calculated

--- src/app/financiamento/financiamento.component.ts
+++ src/app/financiamento/financiamento.component.ts
@@ -22,15 +22,12 @@ export class FinanciamentoComponent {
   private usuario: Usuario;
   private fgtsConfig: FinanciamentoFgtsConfig;
   private financiamentoConfig: FinanciamentoConfig;
-  private grafico: boolean;
-  private fluxoDeCaixa: boolean;
-  private resultado: boolean;
   private calculado: boolean;
   private fgts: boolean;
-  private saveScreen: boolean;
   private isUsingFgts: boolean;
   private posterior: Posterior = Posterior.NaoUsar;
   private configuration: GlobalConfiguration;
+  private selected: string = 'r'
 
   private extrato: Array<ExtratoFinanciamento> = [];
 
@@ -41,62 +38,21 @@ export class FinanciamentoComponent {
     this.configuration = new GlobalConfiguration();
     
     this.properties = new AdvancedProperties(this.usuario, this.financiamentoConfig, this.fgtsConfig, null);
-    this.properties.GlobalConfiguration = this.configuration;
-    
-    this.resultado = false;
-    this.fluxoDeCaixa = false;
-    this.grafico = false;
-    this.calculado = false;
-    this.fgts = false;
-    this.saveScreen = false;
+    this.properties.GlobalConfiguration = this.configuration;  
   }
   onCalcular(user: Usuario) {
-    this.fgts = false;
-    this.grafico = false;
-    this.fluxoDeCaixa = false;
     Object.assign(this.usuario, user);
     this.extrato = this.processorService.Process(this.properties);    
     this.isUsingFgts = this.properties.UsaFgts();
     this.posterior = this.properties.Posterior();
     this.calculado = true;
-    this.resultado = true;
+    this.selected = 'r';
   }
   onFgts(fgts: boolean) {
     this.usuario.usaFGTS = fgts;
   }
-  exibeFluxoDeCaixa(): void {
-    this.fgts = false;
-    this.grafico = false;
-    this.resultado = false;
-    this.saveScreen = false;
-    this.fluxoDeCaixa = true;
-  }
-  exibeResultado(): void {
-    this.fgts = false;
-    this.grafico = false;
-    this.fluxoDeCaixa = false;
-    this.saveScreen = false;
-    this.resultado = true;
-  }
-  exibeGrafico(): void {
-    this.fgts = false;
-    this.fluxoDeCaixa = false;
-    this.resultado = false;
-    this.saveScreen = false;
-    this.grafico = true;
-  }
-  exibeExtrato(): void {
-    this.fluxoDeCaixa = false;
-    this.resultado = false;
-    this.grafico = false;
-    this.fgts = true;
-    this.saveScreen = false;
-  }
-  exibeSaveScreen(): void {
-    this.fluxoDeCaixa = false;
-    this.resultado = false;
-    this.grafico = false;
-    this.fgts = false;
-    this.saveScreen = true;
+
+  private changeScreen(value: string) {
+    this.selected = value;
   }
 }
